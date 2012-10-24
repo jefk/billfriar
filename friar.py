8f8ae556bc877b4f19b88bd28eba4592009fdadc
jefk <kjeffk@gmail.com>
2012-10-24 00:04:19 -0700

    fix faulty logic

--- friar.py
+++ friar.py
@@ -5,7 +5,7 @@ import re
 import heapq
 from collections import Counter
 
-class MaxHeap(list):
+class MaxHeap():
     ''' making heapq classy
     '''
 
@@ -13,20 +13,23 @@ class MaxHeap(list):
         ''' ls is a list of tuples in this format
             [ (val, label), (val, label), ... ]
         '''
-        list.__init__(self, ls)
         # multiply val by -1 to make this into a max heap, heapq is min heap
-        heapq.heapify([ [-1 * val, label] for val, label in self ])
+        self.heap = [ [-1 * val, label] for val, label in ls ]
+        heapq.heapify(self.heap)
 
     def pop(self):
-        entry = heapq.heappop(self)
+        entry = heapq.heappop(self.heap)
         entry[0] = -1 * entry[0]
         return entry
 
     def push(self, entry):
-        heapq.heappush(self, [-1 * entry[0], entry[1] ])
+        heapq.heappush(self.heap, [-1 * entry[0], entry[1] ])
 
     def is_empty(self):
-        return len(self) == 0
+        return len(self.heap) == 0
+
+    def __str__(self):
+        return self.heap.__str__()
 
 
 def parse(line):
@@ -48,33 +51,30 @@ def parse(line):
     return debtor, lender, amount
 
 def make_heaps(credit):
-    debtor_values = [ (credit[person], person) for person in credit if credit[person] < 0 ]
-    lender_values = [ (-1 * credit[person], person) for person in credit if credit[person] > 0 ]
+    debtor_values = [ (-1 * credit[person], person) for person in credit if credit[person] < 0 ]
+    lender_values = [ (credit[person], person) for person in credit if credit[person] > 0 ]
     return MaxHeap(debtor_values), MaxHeap(lender_values)
 
 def shuffle(credit):
     '''
     '''
     debtors, lenders = make_heaps(credit)
-    print(debtors, lenders)
     while not debtors.is_empty() or not lenders.is_empty():
         debtor = debtors.pop()
         lender = lenders.pop()
-        amount = abs(debtor[0] - lender[0])
-        print('debtor,lender', debtor, lender)
+        amount = min(debtor[0], lender[0])
 
         if debtor[0] > lender[0]:
             debtor[0] -= amount
             debtors.push(debtor)
-        elif lender[0] < debtor[0]:
+        elif debtor[0] < lender[0]:
             lender[0] -= amount
             lenders.push(lender)
         else:
             # the debts are equal, so neither goes back on the heap
             pass
 
-        print(debtors, lenders)
-        print('{lender[1]} pays {debtor[1]} ${amount}'.format( **locals() ))
+        print('{debtor[1]} pays {lender[1]} ${amount}'.format( **locals() ))
 
 if __name__ == "__main__":
     credit = Counter()
