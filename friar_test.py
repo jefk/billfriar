b8ff0efd9baa89c7c4d39c44bbf827f61adf9878
jefk <speedracer111@gmail.com>
2013-02-05 22:31:23 -0800

    fix indents

--- friar_test.py
+++ friar_test.py
@@ -4,20 +4,20 @@ import friar
 
 class TestFriar(unittest.TestCase):
 
-  def test_parse_valid_input(self):
-    # debtor, lender, amount, memo = friar.parse('jeff owes ryan 1000000 for a space station')
-    parsed = friar.parse('jeff owes ryan 1000000 for a space station')
-    self.assertEqual(parsed, ('jeff', 'ryan', 1000000, 'for a space station') )
+    def test_parse_valid_input(self):
+        # debtor, lender, amount, memo = friar.parse('jeff owes ryan 1000000 for a space station')
+        parsed = friar.parse('jeff owes ryan 1000000 for a space station')
+        self.assertEqual(parsed, ('jeff', 'ryan', 1000000, 'for a space station') )
 
-  def test_parse_invalid_input(self):
-    invalid_input = ''
-    self.assertRaises(Exception, friar.parse, invalid_input)
+    def test_parse_invalid_input(self):
+        invalid_input = ''
+        self.assertRaises(Exception, friar.parse, invalid_input)
 
-    invalid_input = 'this is not in the grammar'
-    self.assertRaises(Exception, friar.parse, invalid_input)
+        invalid_input = 'this is not in the grammar'
+        self.assertRaises(Exception, friar.parse, invalid_input)
 
-    invalid_input = 'jeff owes ryan ten dollars'
-    self.assertRaises(Exception, friar.parse, invalid_input)
+        invalid_input = 'jeff owes ryan ten dollars'
+        self.assertRaises(Exception, friar.parse, invalid_input)
 
 if __name__ == "__main__":
-  unittest.main()
+    unittest.main()
